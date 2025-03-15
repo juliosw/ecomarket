57cbd8bcbb924f0c013593120958aca39465ab61
Julio Swytka <julio.swytka@gmail.com>
2025-03-15 18:44:31 -0300

    Ajustes

--- api2/app/main.py
+++ api2/app/main.py
@@ -10,12 +10,12 @@ app = FastAPI()
 async def get_external_products(request: Request):
     request_id = request.headers.get("X-Request-ID")
     source_ip = request.headers.get("X-Source-IP")
-    
+
     async with httpx.AsyncClient() as client:
         response = await client.get(
             "https://api.publicapis.org/entries"
         )
-        
+
         return {
             "response_id": str(uuid.uuid4()),
             "original_request_id": request_id,
@@ -23,4 +23,3 @@ async def get_external_products(request: Request):
             "destination_ip": source_ip,
             "data": response.json()
         }
-        

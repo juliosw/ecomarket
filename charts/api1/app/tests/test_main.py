1f220af3f402b55f895e1ef39f613bfffdfaeccc
Julio Swytka <julio.swytka@gmail.com>
2025-03-17 13:19:21 -0300

    Ajustes test_main.py

--- charts/api1/app/tests/test_main.py
+++ charts/api1/app/tests/test_main.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from ..main import app  # Import relativo a partir do diretório pai (app/)
+from main import app  # Import relativo a partir do diretório pai (app/)
 
 
 client = TestClient(app)
